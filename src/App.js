2b398972bb0fa0b7ab2ad1d5f9b0386dd03d7760
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-20 15:36:30 +0100

    understanding website state condition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const products = [
   { name: "laotop", price: "679000" },
@@ -8,11 +9,16 @@ const products = [
   { name: "tablet", price: "23000" }
 ]
 
+
 function App() {
   return (
-    <div>
-      {products.map(product => <Product name={product.name} price={product.price}></Product>)}
+    /* understainding state */
+    <div style={{ textAlign: 'center' }}>
+      <Counter></Counter>
     </div>
+    // <div>
+    //   {products.map(product => <Product name={product.name} price={product.price}></Product>)}
+    // </div>
     // <div>
     //   <Product name="laptop" price="69000"></Product>
     //   <Product name="mobile" price="49000"></Product>
@@ -20,14 +26,32 @@ function App() {
     // </div>
   );
 }
+// Component1
+// function Product(props) {
+//   return (
+//     <div className="product">
+//       <h1>Name:{props.name}</h1>
+//       <h1>Price:{props.price}</h1>
+//     </div>
+//   )
+// }
 
-function Product(props) {
+// component2
+function Counter() {
+  const [count, setCount] = useState(0); //useState() will give an array[value, function]
+
+  const increaseCount = () => (setCount(count + 1))
+  const decreaseCount = () => (setCount(count - 1))
+
   return (
-    <div className="product">
-      <h1>Name:{props.name}</h1>
-      <h1>Price:{props.price}</h1>
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount} >Increase</button>
+      <button style={{ marginLeft: 10 }} onClick={decreaseCount}>Decrease</button>
     </div>
   )
+
 }
+
 
 export default App;
